15b90def880b45beac69f7ab986dfc40ccd5bb68
Andres Preciado <sjaguarsf@gmail.com>
2019-11-27 00:22:51 -0800

    final

--- hw12.py
+++ hw12.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 
 import concurrent.futures
-import math
-import os
-import threading
 import sys
+import codecs
 
-def encode_pathname():
-    pool = concurrent.futures.ThreadPoolExecutor()
-    for i in range(len(sys.argv)):
+def check_utf(path):
+    """
+    Tests provided file to check if it is utf-8 encoded
+    """
+    try:
+        test = codecs.open(path, encoding='utf-8', errors='strict')
+        test.readline()
+        print(f'{path} is utf-8 encoded')
+        pass
+    except UnicodeDecodeError:
+        print(f'SORRY {path} is NOT utf-8 encoded')
+
+def threads(inputs):
+    """
+    Creates multiple threads based off of provided iterable to check_utf function
+    """
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        pool.map(check_utf,inputs)
+
 
 if __name__=="__main__":
-    inputs = range(1,10)
-    pool = concurrent.futures.ThreadPoolExecutor()
-    print(list(pool.map(math.log, inputs)),
-        list(zip(inputs,pool.map(math.log,inputs))))
+    #Create a list of filepaths from arguments
+    args = []
+    for i in range(len(sys.argv)):
+        if i != 0:
+            args.append(sys.argv[i])
+    #Calls threads() with list of filepaths from arguments
+    threads(args)
+
+
 
 
